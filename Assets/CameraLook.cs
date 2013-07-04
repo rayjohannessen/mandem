b11188ca6b10c22c11708d9968794a3ddff85c30
Rantaak@gmail.com <Rantaak@gmail.com@75450360-0e29-0c65-5302-29b3698f876b>
2013-07-04 22:32:40 +0000

    Smoothed the camera out, trying to get collision detection to work with the door cylinders.  Then I'll figure out why I don't understand typecasting in GOC architecture.

--- Assets/CameraLook.cs
+++ Assets/CameraLook.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class CameraLook : MonoBehaviour {
-
+	
+	public GameObject playerHandle;
+	public float cameraFollowDistance;
+	public float cameraFollowHeight;
+	
 	// Use this for initialization
 	void Start () {
-	
+		playerHandle = (GameObject.Find ("Player"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	transform.LookAt((GameObject.Find ("Cube")).transform.position);
+		Vector3 destination = new Vector3(playerHandle.transform.position.x, playerHandle.transform.position.y, playerHandle.transform.position.z);
+		destination += playerHandle.transform.forward * -1.0f * cameraFollowDistance;
+		destination.y += cameraFollowHeight;
+		
+	//transform.translate
+	transform.position += (destination - transform.position) * 0.5f * Time.deltaTime;
+	transform.LookAt(playerHandle.transform.position);
+
 	}
 }
