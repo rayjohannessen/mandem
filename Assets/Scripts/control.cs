fe9a417ba792500feb8d07af2acaeed68ee33b3b
Rantaak@gmail.com <Rantaak@gmail.com@75450360-0e29-0c65-5302-29b3698f876b>
2013-08-06 12:17:01 +0000

    Improvements were made, moving the player movement code over to use CharacterController collision instead.  The player now runs around and can move through doors and this is reflected correctly between the server and the client EXCEPT
    The player moves really friggin fast on the client.

--- Assets/Scripts/control.cs
+++ Assets/Scripts/control.cs
@@ -7,6 +7,8 @@ public class control : MonoBehaviour {
 	public string state; // SEEKING, IDLE
 	public GameObject p_cam;
 	
+	CharacterController controller;
+	
 	CameraLook p_camScript;
 	
 	// Use this for initialization
@@ -15,28 +17,44 @@ public class control : MonoBehaviour {
 		p_cam = GameObject.Find ("Main Camera");
 		p_camScript = p_cam.GetComponent<CameraLook>();
         p_camScript.SetPlayer(gameObject);
+		
 
         door[] doors = FindObjectsOfType(typeof(door)) as door[];
         foreach (door d in doors)
         {
             d.SetPlayer(gameObject);
         }
+		
+		controller = GetComponent<CharacterController>();
 	}
 	
 	public void Teleport (Vector3 tov, Vector3 toq)
 	{
-		rigidbody.velocity = Vector3.zero; // reverse force
-		transform.position = tov;
-		transform.rotation = Quaternion.LookRotation(toq);
-		rigidbody.velocity = transform.forward * 2.0f; // apply force
+		//rigidbody.velocity = Vector3.zero; // reverse force
+		controller.transform.position = tov;
+		controller.transform.rotation = Quaternion.LookRotation(toq);
+		//rigidbody.velocity = transform.forward * 2.0f; // apply force
 		state = "idle";
 	}
 	
-	void OnCollisionEnter(Collision other)	
+	void OnControllerColliderHit(ControllerColliderHit other)
 	{
 		if(other.gameObject.name.Substring(0, 4) == "door")
 		{
 			//state = "idle";
+			Transform DoorA = other.collider.gameObject.transform;
+			Transform DoorB = other.collider.gameObject.GetComponent<door>().partner.transform;
+			
+			if (Vector3.Dot(transform.forward, DoorA.forward) < 0.0f) 
+			{
+			    Vector3 vel = transform.forward;
+			    vel = DoorA.InverseTransformDirection (vel);
+			    vel = DoorB.TransformDirection (vel);
+			    vel = -vel;
+    					
+			    Teleport(DoorB.position, vel);
+			    p_camScript.tele(); // update camera position to new player position
+			}
 		}
 		
 		if(other.gameObject.name == "wall")
@@ -44,6 +62,7 @@ public class control : MonoBehaviour {
 			state = "idle";	
 		}
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
@@ -66,16 +85,16 @@ public class control : MonoBehaviour {
 					if (oinfo.collider.name.Substring(0, 4) == "door") // if we click on a door, make sure to head to the position of the door.
 					{
 						target = oinfo.collider.gameObject.transform.position;
-						target.y = transform.position.y;
+						target.y = controller.transform.position.y;
 					}
 					else
 					{
 			        	target = ray.GetPoint(oinfo.distance); // set our destination to wherever on the ground the mouse has been clicked
-						target.y = transform.position.y;					
+						target.y = controller.transform.position.y;					
 					}
 					
 					speed = 50.0f;
-					transform.LookAt(target);
+					controller.transform.LookAt(target);
 					state = "seeking";
 			    }
 			}
@@ -84,19 +103,20 @@ public class control : MonoBehaviour {
 		else if (Input.GetMouseButton (0))
 		{
 			float mouseturnspeed = 10.0f;
-			transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * mouseturnspeed);
+			controller.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * mouseturnspeed);
 			state = "idle";
 		}
 		
 		//accelerate toward destination while five feet away
 
-			float distanceFromTarget = Vector3.Distance(transform.position, target);
+			float distanceFromTarget = Vector3.Distance(controller.transform.position, target);
 		
 			if (state == "seeking")
 			{
 				//rigidbody.AddForce(transform.forward * speed * Time.deltaTime);
 				//transform.position += transform.forward * speed * Time.deltaTime * 0.01f;
-                transform.Translate(transform.forward * speed * Time.deltaTime);
+                //transform.Translate(transform.forward * speed * Time.deltaTime);
+				controller.Move(controller.transform.forward * speed * Time.deltaTime);
 			}
 		
 			if (distanceFromTarget < 0.5f)
