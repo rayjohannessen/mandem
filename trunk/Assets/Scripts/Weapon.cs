e67270efe582486a157f18ce9cc7465117416428
allthingscandid@gmail.com <allthingscandid@gmail.com@75450360-0e29-0c65-5302-29b3698f876b>
2013-09-12 06:21:19 +0000

    start of items

---
+++ trunk/Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Weapon : Item
+{
+    public enum eWeaponType { WT_KNIFE, NUM_WEAPON_TYPES }
+
+    public eWeaponType weaponType;
+
+    public void SetProperties(eItemType _itemType, int _id, eWeaponType _weaponType)
+    {
+        base.SetBaseProperties(_itemType, _id);
+
+        weaponType = _weaponType;
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
