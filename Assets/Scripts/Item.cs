dd8b33fc001adff735dbb94862604f762930496d
allthingscandid@gmail.com <allthingscandid@gmail.com@75450360-0e29-0c65-5302-29b3698f876b>
2013-09-12 06:21:19 +0000

    start of items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    public enum eItemType { IT_WEAPON, IT_POTION, IT_CONTAINER, IT_MONEY, IT_JEWELRY, NUM_ITEM_TYPES }
+
+    public eItemType itemType;
+
+    public int id;
+
+    public void SetBaseProperties(eItemType _type, int _id)
+    {
+        itemType = _type;
+        id = _id;
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
